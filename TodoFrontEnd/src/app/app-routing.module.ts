b289d4ceebe9dda69550a6026b206491b0accc43
Yixi <lyh1112@gmail.com>
2023-10-23 17:52:24 +0800

    feat: add a router for todo list and todo detail

--- TodoFrontEnd/src/app/app-routing.module.ts
+++ TodoFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: TodoListComponent,
+  },
+  {
+    path: 'detail', component: TodoDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TodoFrontEnd/src/app/app.component.spec.ts
+++ TodoFrontEnd/src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterTestingHarness, RouterTestingModule } from '@angular/router/testing';
+import {routes} from './app-routing.module'
+import { Router, RouterLink } from '@angular/router';
 import { AppComponent } from './app.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
+import { By } from '@angular/platform-browser';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule.withRoutes(routes)],
     declarations: [AppComponent]
-  }));
+  }).compileComponents());
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,10 +25,29 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('TodoFrontEnd');
   });
 
-  it('should render title', () => {
+  it('should render TodoListCompnoent when navigate to "/"', async () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TodoFrontEnd app is running!');
-  });
+    const app = fixture.componentInstance;
+    const harness = await RouterTestingHarness.create()
+    await harness.navigateByUrl('/')
+    fixture.detectChanges()
+
+    const component = fixture.debugElement.queryAll(By.directive(TodoListComponent))
+
+    expect(component.length).toEqual(1)
+
+  })
+
+  it('should render TodoDetailComponent when navigate to "/detail"', async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const harness = await RouterTestingHarness.create()
+    await harness.navigateByUrl('/detail')
+    fixture.detectChanges()
+
+    const component = fixture.debugElement.queryAll(By.directive(TodoDetailComponent))
+
+    expect(component.length).toEqual(1)
+  })
+
 });
