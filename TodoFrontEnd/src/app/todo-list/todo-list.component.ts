dd04c2e06821c11927931b02a0cbbba8779a90c8
Yixi <lyh1112@gmail.com>
2023-10-24 09:41:58 +0800

    feat: add a todo list show todo item logic

--- TodoFrontEnd/src/app/todo-list/todo-list.component.ts
+++ TodoFrontEnd/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDoItem } from '../model/TodoItem';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
+  todoItems: ToDoItem[] = [
+    new ToDoItem(1, 'Buy Milk', "Buy some Milk", false),
+    new ToDoItem(2, 'Buy Bread', "Buy some Bread", false),
+  ]
 
+  onUpdateTodo(id: number, isDone: boolean) {
+    const todoItem = this.todoItems.find(todo => todo.id === id);
+    if (todoItem) {
+      todoItem.isDone = isDone;
+    }
+  }
 }
